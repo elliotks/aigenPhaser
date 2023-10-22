cd326dec23086ed8832cdcada298e52fede9f0bf
elliotks <elliotks@gmail.com>
2023-10-22 18:15:53 -0500

    POC 02

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io').listen(server);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.use(express.static('public'));
 
